ffd904e60c6c12752507ac50041ffe3af34aa1ba
Karlatemp <karlatemp@vip.qq.com>
2021-07-29 19:04:49 +0800

    docs: configs

---
+++ docs/.conf/nav.js
@@ -0,0 +1,9 @@
+module.exports = {
+    text: 'mirai-console-loader',
+    link: '/',
+    items: [
+        { text: 'Index', link: '/' },
+        { text: 'Script', link: '/Script.html' },
+        { text: 'Plugin', link: '/Plugin.html' },
+    ],
+};
